228b51eddb54e233ee2c9125402beaa166794b73
chenmichael2 <chenmichael2012@gmail.com>
2022-02-21 21:15:06 -0800

    commiting the final answer

--- javascript/excel-sheet-column-number.js
+++ javascript/excel-sheet-column-number.js
@@ -1,14 +1,14 @@
-titleToNumber('ABA');
-
 var titleToNumber = function(columnTitle) {
+    console.log('input: ', columnTitle)
     let letterArr = ['0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     let answer = 0;
     let splitColumnTitle = columnTitle.split('');
     let reverseTitle = splitColumnTitle.reverse();
-    console.log(reverseTitle);
     for (let i = 0; i < reverseTitle.length; i++) {
         let number = letterArr.indexOf(reverseTitle[i]);
         answer += (Math.pow(26, i) * number);
     }
-    console.log(answer);
+    return answer;
 };
+
+console.log('output: ', titleToNumber('ABA'));
