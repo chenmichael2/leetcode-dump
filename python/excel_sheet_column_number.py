deeb9eb50e9fafe2b3b1d5e3eb6a52ba7e7b5763
chenmichael2 <chenmichael2012@gmail.com>
2022-02-21 21:57:40 -0800

    getting rid of print

--- python/excel_sheet_column_number.py
+++ python/excel_sheet_column_number.py
@@ -1,6 +1,6 @@
 def title_to_number(columnTitle):
     print('input', columnTitle)
     letterArr = ['0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    print(letterArr)
+    
 
 title_to_number('ABA')
