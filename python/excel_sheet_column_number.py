f25097b75ae808d9d6237707f473c2e1437ad947
chenmichael2 <chenmichael2012@gmail.com>
2022-02-21 22:23:48 -0800

    finish converting excel sheet ito python

--- python/excel_sheet_column_number.py
+++ python/excel_sheet_column_number.py
@@ -1,6 +1,14 @@
-def title_to_number(columnTitle):
-    print('input', columnTitle)
+def title_to_number(column_title):
+    print('input', column_title)
     letterArr = ['0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    
+    answer = 0
+    reverse = column_title[::-1]
+    lists = list(reverse)
+    print()
+    for i in range(len(lists)):
+        number = letterArr.index(lists[i])
+        answer += (26 ** i) * number
+    return answer
 
-title_to_number('ABA')
+
+print(title_to_number('ABA'))
