f0829078b6ac44ffe7ffc26628a6422fdd43bab9
chenmichael2 <chenmichael2012@gmail.com>
2022-02-24 21:23:42 -0800

    is this the problem

--- javascript/compare-version-numbers.js
+++ javascript/compare-version-numbers.js
@@ -21,10 +21,10 @@ var compareVersion = function(version1, version2) {
             v2SplitDot[i] = 0;
         }
         if (parseInt(v1SplitDot[i]) != parseInt(v2SplitDot[i])) {
-            if (parseInt(v1SplitDot[i]) > parseInt(v2SplitDot[1])) {
+            if (parseInt(v1SplitDot[i]) > parseInt(v2SplitDot[i])) {
                 console.log(1);
                 return 1;
-            } else if (parseInt(v1SplitDot[i]) < parseInt(v2SplitDot[1])) {
+            } else if (parseInt(v1SplitDot[i]) < parseInt(v2SplitDot[i])) {
                 console.log(-1);
                 return -1;
             }
