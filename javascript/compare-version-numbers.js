7b357bf197f741c3b21d4774f1988fa7eb5df17a
chenmichael2 <chenmichael2012@gmail.com>
2022-02-24 21:10:35 -0800

    cannot parseInt(0) => gives NaN

--- javascript/compare-version-numbers.js
+++ javascript/compare-version-numbers.js
@@ -5,20 +5,25 @@ var compareVersion = function(version1, version2) {
     let v1SplitDot = v1.split('.');
     let v2SplitDot = v2.split('.');
     let largerVerLen = 0;
+    console.log(v1SplitDot);
+    console.log(v2SplitDot);
     if (v1SplitDot.length < v2SplitDot.length) {
         largerVerLen = v2SplitDot.length;
     } else {
         largerVerLen = v1SplitDot.length;
     }
     for (let i = 0; i < largerVerLen; i++) {
-        console.log(v1SplitDot[i]);
-        console.log(v2SplitDot[i]);
+        // console.log(v1SplitDot[i]);
+        // console.log(v2SplitDot[i]);
         if (v1SplitDot[i] === undefined) {
             v1SplitDot[i] = 0;
         } else if (v2SplitDot[i] === undefined) {
             v2SplitDot[i] = 0;
         }
         if (parseInt(v1SplitDot[i]) != parseInt(v2SplitDot[i])) {
+            console.log('going through this if');
+            console.log('first if', parseInt(v1SplitDot[i]))
+            console.log('second if', parseInt(v2SplitDot[1]))
             if (parseInt(v1SplitDot[i]) > parseInt(v2SplitDot[1])) {
                 console.log(1);
                 return 1;
@@ -36,4 +41,5 @@ var compareVersion = function(version1, version2) {
     // }
 };
 
-console.log('INPUT: 1.01, 1.001 ======= OUTPUT: 0 //////// ', compareVersion(1.01, 1.001))
+// console.log('INPUT: 1.01, 1.001 ======= OUTPUT: 0 //////// ', compareVersion(1.01, 1.001))
+console.log('INPUT: 1, 0 ======= OUTPUT: 1 //////// ', compareVersion(1, 0))
