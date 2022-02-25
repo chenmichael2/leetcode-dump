aacfca62f608842abb9ccde6a2d08fb084e05789
chenmichael2 <chenmichael2012@gmail.com>
2022-02-24 20:33:14 -0800

    spliting by dot to create Arr

--- javascript/compare-version-numbers.js
+++ javascript/compare-version-numbers.js
@@ -1,6 +1,11 @@
 
 var compareVersion = function(version1, version2) {
-    
+    let v1 = version1.toString();
+    let v2 = version2.toString();
+    let v1SplitDot = v1.split('.');
+    let v2SplitDot = v2.split('.');
+    console.log(v1SplitDot);
+    console.log(v2SplitDot);
 };
 
 console.log('INPUT: 1.01, 1.001 ======= OUTPUT: 0 //////// ', compareVersion(1.01, 1.001))
