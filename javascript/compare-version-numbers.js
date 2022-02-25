2ad48c3b0c3b4e2ee6f33f53503bba860829a389
chenmichael2 <chenmichael2012@gmail.com>
2022-02-24 20:27:10 -0800

    commiting the new function and cleanup

--- javascript/compare-version-numbers.js
+++ javascript/compare-version-numbers.js
@@ -1,5 +1,6 @@
+
 var compareVersion = function(version1, version2) {
     
 };
 
-console.log(compareVersion(1.01, 1.001))
+console.log('INPUT: 1.01, 1.001 ======= OUTPUT: 0 //////// ', compareVersion(1.01, 1.001))
