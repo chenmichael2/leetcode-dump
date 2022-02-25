9273de455d8db7061c65363f70e0881076a0fad7
chenmichael2 <chenmichael2012@gmail.com>
2022-02-24 20:51:24 -0800

    checking to see if i did it

--- javascript/compare-version-numbers.js
+++ javascript/compare-version-numbers.js
@@ -7,9 +7,25 @@ var compareVersion = function(version1, version2) {
     if (v1SplitDot.length !== v2SplitDot.length) {
         return 0;
     }
-    for (let i = 0; i < v1.length; i++) {
-        
+    for (let i = 0; i < v1SplitDot.length; i++) {
+        console.log(v1SplitDot[i]);
+        console.log(v2SplitDot[i]);
+        if (parseInt(v1SplitDot[i]) != parseInt(v2SplitDot[i])) {
+            if (parseInt(v1SplitDot[i]) > parseInt(v2SplitDot[1])) {
+                console.log(1);
+                return 1;
+            } else if (parseInt(v1SplitDot[i]) < parseInt(v2SplitDot[1])) {
+                console.log(-1);
+                return -1;
+            }
+        }
     }
+    return 0;
+    // if (parseInt(v1SplitDot[1]) == parseInt(v2SplitDot[1])) {
+    //     console.log("they are the same");
+    // } else {
+    //     console.log("NOT THE SAME")
+    // }
 };
 
 console.log('INPUT: 1.01, 1.001 ======= OUTPUT: 0 //////// ', compareVersion(1.01, 1.001))
