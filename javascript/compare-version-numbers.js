108da738ddbe816fd8f92acb94494ee4e1db6215
chenmichael2 <chenmichael2012@gmail.com>
2022-02-24 21:11:12 -0800

    parseInt of 0 is Nan

--- javascript/compare-version-numbers.js
+++ javascript/compare-version-numbers.js
@@ -21,9 +21,6 @@ var compareVersion = function(version1, version2) {
             v2SplitDot[i] = 0;
         }
         if (parseInt(v1SplitDot[i]) != parseInt(v2SplitDot[i])) {
-            console.log('going through this if');
-            console.log('first if', parseInt(v1SplitDot[i]))
-            console.log('second if', parseInt(v2SplitDot[1]))
             if (parseInt(v1SplitDot[i]) > parseInt(v2SplitDot[1])) {
                 console.log(1);
                 return 1;
