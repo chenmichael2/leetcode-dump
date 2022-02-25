c9b5c28b9a122d0871d51d5dfb2983dc9215fb54
chenmichael2 <chenmichael2012@gmail.com>
2022-02-24 20:34:57 -0800

    if they are different lengths return0

--- javascript/compare-version-numbers.js
+++ javascript/compare-version-numbers.js
@@ -4,8 +4,9 @@ var compareVersion = function(version1, version2) {
     let v2 = version2.toString();
     let v1SplitDot = v1.split('.');
     let v2SplitDot = v2.split('.');
-    console.log(v1SplitDot);
-    console.log(v2SplitDot);
+    if (v1SplitDot.length !== v2SplitDot.length) {
+        return 0;
+    }
 };
 
 console.log('INPUT: 1.01, 1.001 ======= OUTPUT: 0 //////// ', compareVersion(1.01, 1.001))
