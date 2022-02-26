d16555a93f81a1a37ae34afca186332a33cd23e1
chenmichael2 <chenmichael2012@gmail.com>
2022-02-26 14:18:46 -0800

    finished variables

--- javascript/node-tree.js
+++ javascript/node-tree.js
@@ -3,6 +3,7 @@ function countInserts(nums) {
     let root = 0;
     let right = 0;
     let left = 0;
+    
 }
 
 console.log('INPUT = [2, 1, 3, 4, 2]', 'OUTPUT = 0 1 2 4 6', countInserts([2, 1, 3, 4, 2]));
