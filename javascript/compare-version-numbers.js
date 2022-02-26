4ddda0581be6b6d7beab17239a6bea18a9ea6132
chenmichael2 <chenmichael2012@gmail.com>
2022-02-25 19:58:39 -0800

    cleaning up

--- javascript/compare-version-numbers.js
+++ javascript/compare-version-numbers.js
@@ -35,7 +35,7 @@ var compareVersion = function(version1, version2) {
     //     console.log("they are the same");
     // } else {
     //     console.log("NOT THE SAME")
-    // }
+
 };
 
 // console.log('INPUT: 1.01, 1.001 ======= OUTPUT: 0 //////// ', compareVersion(1.01, 1.001))
