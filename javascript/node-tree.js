b88434348318c45e9cafab5c15c2c0da05d34cf0
chenmichael2 <chenmichael2012@gmail.com>
2022-02-26 14:17:58 -0800

    commiting right

--- javascript/node-tree.js
+++ javascript/node-tree.js
@@ -1,6 +1,7 @@
 function countInserts(nums) {
     let counter = 0;
     let root = 0;
+    let right = 0;
 }
 
 console.log('INPUT = [2, 1, 3, 4, 2]', 'OUTPUT = 0 1 2 4 6', countInserts([2, 1, 3, 4, 2]));
