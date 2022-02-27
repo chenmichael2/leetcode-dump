453a5d53059828baea00bab5d078fcfb07dcdee6
chenmichael2 <chenmichael2012@gmail.com>
2022-02-26 19:43:02 -0800

    answer

--- javascript/maximum-width-binary-tree.js
+++ javascript/maximum-width-binary-tree.js
@@ -2,4 +2,4 @@ function widthOfBinaryTree(root) {
 
 }
 
-console.log('Input: ', root = [1,3,2,5,3,null,9], 'Output: ')
+console.log('Input: ', root = [1,3,2,5,3,null,9], 'Output: ', 4, widthOfBinaryTree([1,3,2,5,3,null,9]))
