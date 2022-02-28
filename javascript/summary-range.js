ca68d70d415cf1b7a50d45ae3c626e84e4664022
chenmichael2 <chenmichael2012@gmail.com>
2022-02-27 18:41:02 -0800

    adding second range

--- javascript/summary-range.js
+++ javascript/summary-range.js
@@ -3,4 +3,4 @@ function summaryRanges(nums) {
 }
 
 console.log('Input: ', nums = [0,1,2,4,5,7], 'Output: ', ["0->2","4->5","7"], summaryRanges([0,1,2,4,5,7]))
-console.log('Input: ', nums = [0,2,3,4,6,8,9], 'Output: ', ["0->2","4->5","7"], summaryRanges([0,1,2,4,5,7]))
+console.log('Input: ', nums = [0,2,3,4,6,8,9], 'Output: ', ["0","2->4","6","8->9"], summaryRanges([0,1,2,4,5,7]))
