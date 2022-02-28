17e285d04feb3cc7072056fbd11ae46d8d8fa0a9
chenmichael2 <chenmichael2012@gmail.com>
2022-02-27 19:01:19 -0800

    started but not working

--- javascript/summary-range.js
+++ javascript/summary-range.js
@@ -1,7 +1,23 @@
 function summaryRanges(nums) {
+    let output = [];
     for (let i = 0; i < nums.length; i++) {
-        let x = nums[0];
+        let j = i + 1;
+        let string = "";
+        console.log('i', i);
+        console.log('j', j);
+        while (nums[i] + 1 === nums[j]) {
+            j++;
+            i++;
+        }
+        if (i != j) {
+            string = i + "->" + j;
+        } else {
+            string = i;
+        }
+        output.push(string);
+        i = j;
     }
+    return output;
 }
 
 // console.log('Input: ', nums = [0,1,2,4,5,7], 'Output: ', ["0->2","4->5","7"], summaryRanges([0,1,2,4,5,7]))
