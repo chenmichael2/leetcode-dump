66d15faa5b288a09f006975485d866123c3ad6b8
chenmichael2 <chenmichael2012@gmail.com>
2022-03-11 21:05:10 -0800

    print spelled correctly

--- python/palindrome_number.py
+++ python/palindrome_number.py
@@ -2,7 +2,7 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         string = str(x)
         print(string, len(string))
-        pring(string[0])
+        print(string[0])
     
 s = Solution()
 
