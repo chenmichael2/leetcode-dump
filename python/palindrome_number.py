9e036cfeb655f48bc5561feb569044047585130e
chenmichael2 <chenmichael2012@gmail.com>
2022-03-11 21:13:32 -0800

    defining the length variable

--- python/palindrome_number.py
+++ python/palindrome_number.py
@@ -1,9 +1,10 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         string = str(x)
-        print(string, len(string))
+        length = len(string)
+        print(string, length)
         print(string[1])
-        if (len(string) == 1):
+        if (length == 1):
             return False
         else:
             print("longer than 1")
