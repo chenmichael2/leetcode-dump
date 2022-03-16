cfe1cbaa369e8866aa21330d709c1e8f783cc390
chenmichael2 <chenmichael2012@gmail.com>
2022-03-15 22:55:22 -0700

    defining Binary Search Tree

--- javascript/node-tree.js
+++ javascript/node-tree.js
@@ -41,7 +41,9 @@ function countInserts(nums) {
         }
     }
 
-    class
+    class BST {
+        
+    }
 }
 
 console.log('INPUT = [2, 1, 3, 4, 2]', 'OUTPUT = 0 1 2 4 6', countInserts([2, 1, 3, 4, 2]));
