1899dfe966977aecf95399da4c396b29e611236e
chenmichael2 <chenmichael2012@gmail.com>
2022-03-15 22:54:37 -0700

    adding a new class

--- javascript/node-tree.js
+++ javascript/node-tree.js
@@ -40,6 +40,8 @@ function countInserts(nums) {
             this.right = null;
         }
     }
+
+    class
 }
 
 console.log('INPUT = [2, 1, 3, 4, 2]', 'OUTPUT = 0 1 2 4 6', countInserts([2, 1, 3, 4, 2]));
