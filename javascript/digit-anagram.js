aa6db2dd4540bbe1ee4ff1f3e6b7a100f9e83ed2
chenmichael2 <chenmichael2012@gmail.com>
2022-03-18 16:45:51 -0700

    fixing the console.log

--- javascript/digit-anagram.js
+++ javascript/digit-anagram.js
@@ -2,4 +2,4 @@ function summaryRanges(a) {
     
 }
 
-console.log('Input: ', [23, 57, 75, 832, 328, 382, 567], 'Output: ', 4, summaryRanges([23, 57, 75, 832, 328, 382, 567]);
+console.log('Input: ', [23, 57, 75, 832, 328, 382, 567], 'Output: ', 4, summaryRanges([23, 57, 75, 832, 328, 382, 567]));
