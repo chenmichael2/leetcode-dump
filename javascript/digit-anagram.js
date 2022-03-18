1ceb7dae2019c972e3e77f5b0e784b7aeccf2776
chenmichael2 <chenmichael2012@gmail.com>
2022-03-18 16:47:15 -0700

    adding the answer

--- javascript/digit-anagram.js
+++ javascript/digit-anagram.js
@@ -1,5 +1,6 @@
 function digit_anagram(a) {
     let count = 0;
+    return count;
 }
 
 console.log('Input: ', [23, 57, 75, 832, 328, 382, 567], 'Output: ', 4, digit_anagram([23, 57, 75, 832, 328, 382, 567]));
