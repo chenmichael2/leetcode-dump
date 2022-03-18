0a4dec1980610a0966b7833f88c3c8f770544930
chenmichael2 <chenmichael2012@gmail.com>
2022-03-18 16:49:11 -0700

    adding one more input

--- javascript/digit-anagram.js
+++ javascript/digit-anagram.js
@@ -1,7 +1,7 @@
 function digit_anagram(a) {
     let count = 0;
-    
+
     return count;
 }
 
-console.log('Input: ', [23, 57, 75, 832, 328, 382, 567], 'Expected Output: ', 4, 'Output', digit_anagram([23, 57, 75, 832, 328, 382, 567]));
+console.log('Input: ', [23, 57, 75, 832, 328, 98 ,382, 567], 'Expected Output: ', 4, 'Output', digit_anagram([23, 57, 98, 75, 832, 328, 382, 567]));
