94a669cf379e54bbfe94f37c989b51cee5a42354
chenmichael2 <chenmichael2012@gmail.com>
2022-03-18 16:46:18 -0700

    recalling the function

--- javascript/digit-anagram.js
+++ javascript/digit-anagram.js
@@ -1,5 +1,5 @@
-function summaryRanges(a) {
+function digit_anagram(a) {
     
 }
 
-console.log('Input: ', [23, 57, 75, 832, 328, 382, 567], 'Output: ', 4, summaryRanges([23, 57, 75, 832, 328, 382, 567]));
+console.log('Input: ', [23, 57, 75, 832, 328, 382, 567], 'Output: ', 4, digit_anagram([23, 57, 75, 832, 328, 382, 567]));
