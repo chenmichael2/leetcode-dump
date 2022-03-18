d50385cacf5da45e267befc9687dd9e7cbb5dd1e
chenmichael2 <chenmichael2012@gmail.com>
2022-03-18 16:44:46 -0700

    adding the output

--- javascript/digit-anagram.js
+++ javascript/digit-anagram.js
@@ -2,4 +2,4 @@ function summaryRanges(a) {
     
 }
 
-console.log('Input: ', [23, 57, 75, 832, 328, 382, 567], 'Output: ', , summaryRanges([0,1,2,4,5,7]));
+console.log('Input: ', [23, 57, 75, 832, 328, 382, 567], 'Output: ', 4, summaryRanges([0,1,2,4,5,7]));
