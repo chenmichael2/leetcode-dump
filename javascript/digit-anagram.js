d26dcf255e204a6d530d4e6b7c997556d2cbdde7
chenmichael2 <chenmichael2012@gmail.com>
2022-03-18 17:00:16 -0700

    adding a longer number

--- javascript/digit-anagram.js
+++ javascript/digit-anagram.js
@@ -5,4 +5,4 @@ function digit_anagram(a) {
     return count;
 }
 
-console.log('Input: ', [23, 57, 75, 832, 328, 98 ,382, 567], 'Expected Output: ', 4, 'Output', digit_anagram([23, 57, 98, 75, 832, 328, 382, 567]));
+console.log('Input: ', [5432, 23, 57, 75, 832, 328, 98 ,382, 567], 'Expected Output: ', 4, 'Output', digit_anagram([5432, 23, 57, 98, 75, 832, 328, 382, 567]));
