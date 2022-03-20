642a85519d6ab0c02c3924d688d57b809d4b7398
chenmichael2 <chenmichael2012@gmail.com>
2022-03-19 23:43:20 -0700

    commenting out unnecessary things to clean up

--- javascript/remove-duplicate-letters.js
+++ javascript/remove-duplicate-letters.js
@@ -3,4 +3,4 @@ function removeDuplicateLetters(s) {
 }
 
 console.log('Input: ', "s = cbacdcbc" , 'Expected Output: ', "acdb", 'Output', removeDuplicateLetters("cbacdcbc"))
-console.log('Input: ', "s = bcabc" , 'Expected Output: ', "abc", 'Output', removeDuplicateLetters("bcabc"));
+// console.log('Input: ', "s = bcabc" , 'Expected Output: ', "abc", 'Output', removeDuplicateLetters("bcabc"));
