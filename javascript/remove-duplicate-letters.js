386945714615e1eb80fb3029b62665d5fab17aad
chenmichael2 <chenmichael2012@gmail.com>
2022-03-22 22:49:00 -0700

    adding adn cleaning

--- javascript/remove-duplicate-letters.js
+++ javascript/remove-duplicate-letters.js
@@ -1,16 +1,12 @@
 function removeDuplicateLetters(s) {
-    console.log(s);
-    let string = s;
-    let firstLetter = s[1];
     let cnt = Array(26).fill(0);
     let visit = Array(26).fill(false);
     // for (let i = 0; i < string.length; i++) {
     //     console.log(s[i], s.charCodeAt(i));
     // }
-    for (var i = 0; i < n; i++) {
+    for (var i = 0; i < s.length; i++) {
         cnt[s[i].charCodeAt(0) - 'a'.charCodeAt(0)]++;
     }
-    console.log(cnt);
 }
 
 console.log('Input: ', "s = cbacdcbc" , 'Expected Output: ', "acdb", 'Output', removeDuplicateLetters("cbacdcbc"))
