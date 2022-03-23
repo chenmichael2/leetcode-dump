19a017f4bff029a8fd2fc7f1900d60f4a901e28d
chenmichael2 <chenmichael2012@gmail.com>
2022-03-22 22:46:36 -0700

    seeing the cnt

--- javascript/remove-duplicate-letters.js
+++ javascript/remove-duplicate-letters.js
@@ -4,12 +4,13 @@ function removeDuplicateLetters(s) {
     let firstLetter = s[1];
     let cnt = Array(26).fill(0);
     let visit = Array(26).fill(false);
-    console.log(visit);
-    console.log(cnt);
     // for (let i = 0; i < string.length; i++) {
     //     console.log(s[i], s.charCodeAt(i));
     // }
-    
+    for (var i = 0; i < n; i++) {
+        cnt[s[i].charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+    console.log(cnt);
 }
 
 console.log('Input: ', "s = cbacdcbc" , 'Expected Output: ', "acdb", 'Output', removeDuplicateLetters("cbacdcbc"))
