6ad051f1977661680e6dfb5832b8695435fc8c90
chenmichael2 <chenmichael2012@gmail.com>
2022-03-25 19:34:46 -0700

    adding a new if statement

--- javascript/binary-search.js
+++ javascript/binary-search.js
@@ -2,13 +2,18 @@ var search = function(nums, target) {
     console.log(nums, target);
     let lastIndex = nums.length - 1;
     console.log(lastIndex);
+    if (nums.includes(target)) {
+        console.log('first if statement')
+        let index = 
+    }
     if (target > num[lastIndex] || target < num[0]) {
         console.log('not within search');
         return -1;
     }
     function searchHalf(arr, target) {
-         
+        let medianIndex = Math.floor(arr.length);
+        if (target < num[medianIndex])
     }
 };
 
-console.log(search([-1,0,3,5,9,12], 4))
+console.log(search([-1,0,3,4,5,9,12], 4))
