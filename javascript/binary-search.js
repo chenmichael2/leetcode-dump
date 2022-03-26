cab2b0535eb630e3cd9dab1f2c6dedfa926de807
chenmichael2 <chenmichael2012@gmail.com>
2022-03-25 20:17:15 -0700

    nevermind its this easy

--- javascript/binary-search.js
+++ javascript/binary-search.js
@@ -1,20 +1,9 @@
 //# 704
 var search = function(nums, target) {
-    console.log(nums, target);
-    let lastIndex = nums.length - 1;
-    console.log(lastIndex);
     if (nums.includes(target)) {
-        console.log('first if statement')
-        let index = nums.indexOf(target);
-        console.log(index);
-    }
-    if (target > nums[lastIndex] || target < nums[0]) {
-        console.log('not within search');
+        return nums.indexOf(target);
+    } else {
         return -1;
-    }
-    function searchHalf(arr, target) {
-        let medianIndex = Math.floor(arr.length);
-        // if (target < num[medianIndex])
     }
 };
 
