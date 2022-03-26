ef46d5ebb78edc9829f21e6f030b89d097f93fb5
chenmichael2 <chenmichael2012@gmail.com>
2022-03-25 18:54:51 -0700

    adding a variable lastIndex

--- javascript/binary-search.js
+++ javascript/binary-search.js
@@ -1,6 +1,8 @@
 var search = function(nums, target) {
     console.log(nums, target);
-    
+    let lastIndex = nums.length - 1;
+    console.log(lastIndex);
+    // if (target > num)
 };
 
 console.log(search([-1,0,3,5,9,12], 4))
