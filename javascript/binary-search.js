cb87266d7242db12f0d7df821003b779ed67c67b
chenmichael2 <chenmichael2012@gmail.com>
2022-03-25 19:37:37 -0700

    this is leetcode 704

--- javascript/binary-search.js
+++ javascript/binary-search.js
@@ -1,3 +1,4 @@
+//# 704
 var search = function(nums, target) {
     console.log(nums, target);
     let lastIndex = nums.length - 1;
@@ -7,14 +8,14 @@ var search = function(nums, target) {
         let index = nums.indexOf(target);
         console.log(index);
     }
-    if (target > num[lastIndex] || target < num[0]) {
+    if (target > nums[lastIndex] || target < nums[0]) {
         console.log('not within search');
         return -1;
     }
     function searchHalf(arr, target) {
         let medianIndex = Math.floor(arr.length);
-        if (target < num[medianIndex])
+        // if (target < num[medianIndex])
     }
 };
 
-console.log(search([-1,0,3,4,5,9,12], 4))
+console.log(search([-1,0,3,4,5,9,12], 4)) // output should be
