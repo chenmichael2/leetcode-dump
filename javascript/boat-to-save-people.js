245a309550344e957a630713def90575d4528c64
chenmichael2 <chenmichael2012@gmail.com>
2022-03-28 20:48:00 -0700

    adding more console.logs

--- javascript/boat-to-save-people.js
+++ javascript/boat-to-save-people.js
@@ -5,3 +5,5 @@ function savePeopleBoat(ppl, lim) {
 }
 
 console.log('Input: ', "people = [1,2], limit = 3" , 'Expected Output: ', 1, 'Output', savePeopleBoat([1,2], 3))
+console.log('Input: ', "people = [1,2], limit = 3" , 'Expected Output: ', 1, 'Output', savePeopleBoat([1,2], 3))
+console.log('Input: ', "people = [1,2], limit = 3" , 'Expected Output: ', 1, 'Output', savePeopleBoat([1,2], 3))
