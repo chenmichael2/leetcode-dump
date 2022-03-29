7504d60aba7ee8515acc05c615b6c0d635fdb21f
chenmichael2 <chenmichael2012@gmail.com>
2022-03-28 20:48:40 -0700

    adding the third input

--- javascript/boat-to-save-people.js
+++ javascript/boat-to-save-people.js
@@ -6,4 +6,4 @@ function savePeopleBoat(ppl, lim) {
 
 console.log('Input: ', "people = [1,2], limit = 3" , 'Expected Output: ', 1, 'Output', savePeopleBoat([1,2], 3))
 console.log('Input: ', "people = [3,2,2,1], limit = 3" , 'Expected Output: ', 1, 'Output', savePeopleBoat([1,2], 3))
-console.log('Input: ', "people = [1,2], limit = 3" , 'Expected Output: ', 1, 'Output', savePeopleBoat([1,2], 3))
+console.log('Input: ', "people = [3,5,3,4], limit = 5" , 'Expected Output: ', 1, 'Output', savePeopleBoat([1,2], 3))
