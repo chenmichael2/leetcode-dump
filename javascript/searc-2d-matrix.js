34779f335b055b8bcdb6bf6fc62dcfd0943036c5
chenmichael2 <chenmichael2012@gmail.com>
2022-03-30 16:56:00 -0700

    adding the function with parameters and console.log

--- javascript/searc-2d-matrix.js
+++ javascript/searc-2d-matrix.js
@@ -1,5 +1,7 @@
 // daily programmer 20220330
 
 function search2dMatrix() {
-    
+
 }
+
+console.log('Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3' , 'Expected Output: true', 'Output: ', search2dMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3));
