29474deff9df90d1d0a58481658c15bac7b0bf1e
chenmichael2 <chenmichael2012@gmail.com>
2022-03-29 17:23:21 -0700

    commenting back in our tests

--- javascript/boat-to-save-people.js
+++ javascript/boat-to-save-people.js
@@ -38,6 +38,6 @@ function savePeopleBoat(ppl, lim) {
     return boat;
 }
 
-// console.log('Input: ', "people = [1,2], limit = 3" , 'Expected Output: ', 1, 'Output', savePeopleBoat([1,2], 3));
-// console.log('Input: ', "people = [3,2,2,1], limit = 3" , 'Expected Output: ', 3, 'Output', savePeopleBoat([3,2,2,1], 3));
+console.log('Input: ', "people = [1,2], limit = 3" , 'Expected Output: ', 1, 'Output', savePeopleBoat([1,2], 3));
+console.log('Input: ', "people = [3,2,2,1], limit = 3" , 'Expected Output: ', 3, 'Output', savePeopleBoat([3,2,2,1], 3));
 console.log('Input: ', "people = [3,5,3,4], limit = 5" , 'Expected Output: ', 4, 'Output', savePeopleBoat([3,5,3,4], 5));
