fc797b2ebafe85ae0252ca549a2844d1f2abdac0
chenmichael2 <chenmichael2012@gmail.com>
2022-03-30 16:57:16 -0700

    adding the second test

--- javascript/searc-2d-matrix.js
+++ javascript/searc-2d-matrix.js
@@ -5,3 +5,4 @@ function search2dMatrix(matrix, target) {
 }
 
 console.log('Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3' , 'Expected Output: true', 'Output: ', search2dMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3));
+console.log('Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13' , 'Expected Output: false', 'Output: ', search2dMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13));
