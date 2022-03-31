bb708a2c016ade3c4932528608e93cb1d5c9b2f7
chenmichael2 <chenmichael2012@gmail.com>
2022-03-30 17:07:38 -0700

    seeing what i can use to manipulate the arrays

--- javascript/searc-2d-matrix.js
+++ javascript/searc-2d-matrix.js
@@ -1,8 +1,8 @@
 // daily programmer 20220330
 
 function search2dMatrix(matrix, target) {
-
+    console.log(matrix.length)
 }
 
 console.log('Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3' , 'Expected Output: true', 'Output: ', search2dMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3));
-console.log('Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13' , 'Expected Output: false', 'Output: ', search2dMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13));
+// console.log('Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13' , 'Expected Output: false', 'Output: ', search2dMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13));
