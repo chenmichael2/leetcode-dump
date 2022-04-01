ff9d9649c9d88811da47554604d0528154af54ed
chenmichael2 <chenmichael2012@gmail.com>
2022-03-31 18:34:23 -0700

    adding the middle array index

--- javascript/searc-2d-matrix.js
+++ javascript/searc-2d-matrix.js
@@ -8,11 +8,11 @@ function search2dMatrix(matrix, target) {
     // binary search
     function findTheArray(matrix, target) {
         // go to the middle array and see if the target is less than or greater than or in the matrix.
-        let middleArr = Math.floor((matrix.length)/2);
-        console.log(middleArr);
+        let middleArrIndex = Math.floor((matrix.length)/2);
+        
     }
     findTheArray(matrix, target);
-    
+
 }
 
 console.log('Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3' , 'Expected Output: true', 'Output: ', search2dMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3));
