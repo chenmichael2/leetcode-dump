0131336621e790e7cd808b92a2ae8a03149014d3
chenmichael2 <chenmichael2012@gmail.com>
2022-04-01 14:30:04 -0700

    the matrix 2d search is working

--- javascript/searc-2d-matrix.js
+++ javascript/searc-2d-matrix.js
@@ -4,7 +4,7 @@ function search2dMatrix(matrix, target) {
     // [] for loop through each array and find the first and last index of each array. 
     // [] see if the target number is in the array or not
     // [] see if the array includes and return true or false
-
+    let answer = null;
     // [] binary search
     function findTheArray(matrix, target, firstArrIndex, lastArrIndex) {
         // go to the middle array and see if the target is less than or greater than or in the matrix.
@@ -18,9 +18,11 @@ function search2dMatrix(matrix, target) {
             console.log("i'm in this array");
             // [] find in this array
             if (matrix[middleArrIndex].includes(target)) {
-                return true;
+                console.log("this if", matrix[middleArrIndex].indexOf(target));
+                answer = true
             } else {
-                return false;
+                console.log("this else")
+                answer = false;
             }
         } else {
             console.log("I still need to find the array")
@@ -35,8 +37,8 @@ function search2dMatrix(matrix, target) {
         }
     }
     findTheArray(matrix, target, 0, matrix.length - 1);
-
+    return answer;
 }
 
-console.log('Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3' , 'Expected Output: true', 'Output: ', search2dMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3));
-// console.log('Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13' , 'Expected Output: false', 'Output: ', search2dMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13));
+// console.log('Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3' , 'Expected Output: true', 'Output: ', search2dMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3));
+console.log('Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13' , 'Expected Output: false', 'Output: ', search2dMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13));
