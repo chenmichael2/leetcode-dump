041f5cdbfcd20da94360a89a8d2bb8ecd3c8f64e
chenmichael2 <chenmichael2012@gmail.com>
2022-03-31 17:48:47 -0700

    adding the steps i am going to take

--- javascript/searc-2d-matrix.js
+++ javascript/searc-2d-matrix.js
@@ -1,7 +1,10 @@
 // daily programmer 20220330
 
 function search2dMatrix(matrix, target) {
-    console.log(matrix.length)
+    // for loop through each array and find the first and last index of each array. 
+    // see if the target number is in the array or not
+    //
+
 }
 
 console.log('Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3' , 'Expected Output: true', 'Output: ', search2dMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3));
