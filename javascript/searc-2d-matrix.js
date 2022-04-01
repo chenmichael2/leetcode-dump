161e8d0442cd7034ea29aa29015c23d4572d47a5
chenmichael2 <chenmichael2012@gmail.com>
2022-03-31 18:20:09 -0700

    testing

--- javascript/searc-2d-matrix.js
+++ javascript/searc-2d-matrix.js
@@ -12,6 +12,7 @@ function search2dMatrix(matrix, target) {
         console.log(middleArr);
     }
     findTheArray(matrix, target);
+    
 }
 
 console.log('Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3' , 'Expected Output: true', 'Output: ', search2dMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3));
