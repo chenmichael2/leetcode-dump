484fe32e6f68dcb064c10cad5d138b209f9ca063
chenmichael2 <chenmichael2012@gmail.com>
2022-04-01 22:19:02 -0700

    adding the other inputs

--- javascript/valid-palindrome-2.js
+++ javascript/valid-palindrome-2.js
@@ -5,3 +5,6 @@ function validPalindrome(s) {
 }
 
 console.log('Input: s = "aba"' , 'Expected Output: true', 'Output: ', validPalindrome("aba"));
+// console.log('Input: s = "abac"' , 'Expected Output: false', 'Output: ', validPalindrome("abac"));
+// console.log('Input: s = "abc"' , 'Expected Output: false', 'Output: ', validPalindrome("abc"));
+// console.log('Input: s = "abba"' , 'Expected Output: true', 'Output: ', validPalindrome("abba"));
