37dcd9498f021fc3bbedc0a2afdc97dc065e6386
chenmichael2 <chenmichael2012@gmail.com>
2022-04-01 22:15:07 -0700

    getting rid of previous input

--- javascript/valid-palindrome-2.js
+++ javascript/valid-palindrome-2.js
@@ -4,4 +4,4 @@ function validPalindrome() {
 
 }
 
-console.log('Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13' , 'Expected Output: false', 'Output: ', search2dMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13));
+console.log('Input: ' , 'Expected Output: ', 'Output: ', search2dMatrix());
