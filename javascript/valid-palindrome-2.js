a029ddeab4e2eafd1fa9b1971c25795d63731949
chenmichael2 <chenmichael2012@gmail.com>
2022-04-02 21:08:40 -0700

    fixin ghte middle index and the s in substring

--- javascript/valid-palindrome-2.js
+++ javascript/valid-palindrome-2.js
@@ -6,9 +6,9 @@ function validPalindrome(s) {
         console.log("this is even");
     } else {
         console.log("this is odd");
-        let middleIndex = ((s.length + 1)/2);
+        let middleIndex = ((s.length + 1)/2) - 1;
         console.log(middleIndex);
-        let firstHalf = substr(0, middleIndex - 1);
+        let firstHalf = s.substr(0, middleIndex - 1);
         console.log(firstHalf)
     }
 }
