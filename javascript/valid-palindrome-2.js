854b82d8da1c6a42408d63d68ea412d38019e4d4
chenmichael2 <chenmichael2012@gmail.com>
2022-04-02 21:34:49 -0700

    uncommenting out everything

--- javascript/valid-palindrome-2.js
+++ javascript/valid-palindrome-2.js
@@ -30,8 +30,8 @@ function validPalindrome(s) {
     }
 }
 
-// console.log('Input: s = "aba"' , 'Expected Output: true', 'Output: ', validPalindrome("aba"));
+console.log('Input: s = "aba"' , 'Expected Output: true', 'Output: ', validPalindrome("aba"));
 console.log('Input: s = "abac"' , 'Expected Output: false', 'Output: ', validPalindrome("abac"));
-// console.log('Input: s = "abc"' , 'Expected Output: false', 'Output: ', validPalindrome("abc"));
+console.log('Input: s = "abc"' , 'Expected Output: false', 'Output: ', validPalindrome("abc"));
 console.log('Input: s = "abba"' , 'Expected Output: true', 'Output: ', validPalindrome("abba"));
-// console.log('Input: s = "abaca"' , 'Expected Output: false', 'Output: ', validPalindrome("abaca"));
+console.log('Input: s = "abaca"' , 'Expected Output: false', 'Output: ', validPalindrome("abaca"));
