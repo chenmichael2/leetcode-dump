4e5e850e0b0c0cddb1cd971ecde5aad635e5d519
chenmichael2 <chenmichael2012@gmail.com>
2022-04-02 21:27:56 -0700

    adding the middle index

--- javascript/valid-palindrome-2.js
+++ javascript/valid-palindrome-2.js
@@ -4,7 +4,7 @@ function validPalindrome(s) {
     // [] see if s is even or odd length
     if (s.length % 2 === 0) {
         console.log("this is even");
-
+        let middleIndex = (s.length / 2);
     } else {
         console.log("this is odd");
         let middleIndex = ((s.length + 1)/2) - 1;
