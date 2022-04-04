21bd751177c16231d5465d0d05200d71f4bd404a
chenmichael2 <chenmichael2012@gmail.com>
2022-04-03 22:48:18 -0700

    adding the next input output

--- javascript/swapping-nodes-in-linked-list.js
+++ javascript/swapping-nodes-in-linked-list.js
@@ -3,3 +3,4 @@ function swapNode(head, k) {
 }
 
 console.log('Input: head = [1,2,3,4,5], k = 2' , 'Expected Output: [1,4,3,2,5]', 'Output: ', swapNode([1,2,3,4,5], 2));
+console.log('Input: head = [1,2,3,4,5], k = 2' , 'Expected Output: [1,4,3,2,5]', 'Output: ', swapNode([1,2,3,4,5], 2));
