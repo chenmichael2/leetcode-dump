8cd433618fcd262e203d36d2de07c701b0af1197
chenmichael2 <chenmichael2012@gmail.com>
2022-04-03 22:47:29 -0700

    adding the proper input outputs

--- javascript/swapping-nodes-in-linked-list.js
+++ javascript/swapping-nodes-in-linked-list.js
@@ -2,4 +2,4 @@ function swapNode(head, k) {
 
 }
 
-console.log('Input: "' , 'Expected Output: ', 'Output: ', validPalindrome());
+console.log('Input: head = [1,2,3,4,5], k = 2' , 'Expected Output: [1,4,3,2,5]', 'Output: ', validPalindrome([1,2,3,4,5], 2));
