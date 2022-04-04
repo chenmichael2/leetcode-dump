5632f0edf9e26a48b033454a37442ff2eecb7220
chenmichael2 <chenmichael2012@gmail.com>
2022-04-03 22:53:22 -0700

    adding the second and correcting the consolelog

--- javascript/swapping-nodes-in-linked-list.js
+++ javascript/swapping-nodes-in-linked-list.js
@@ -3,4 +3,4 @@ function swapNode(head, k) {
 }
 
 console.log('Input: head = [1,2,3,4,5], k = 2' , 'Expected Output: [1,4,3,2,5]', 'Output: ', swapNode([1,2,3,4,5], 2));
-console.log('Input: head = [1,2,3,4,5], k = 2' , 'Expected Output: [1,4,3,2,5]', 'Output: ', swapNode([1,2,3,4,5], 2));
+console.log('Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5' , 'Expected Output: [7,9,6,6,8,7,3,0,9,5]', 'Output: ', swapNode([7,9,6,6,7,8,3,0,9,5], 5));
