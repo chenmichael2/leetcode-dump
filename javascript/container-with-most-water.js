fee20d6426979b4f9f8e253e89019d5113b7f12a
chenmichael2 <chenmichael2012@gmail.com>
2022-04-04 21:29:13 -0700

    adding the input and output

--- javascript/container-with-most-water.js
+++ javascript/container-with-most-water.js
@@ -4,4 +4,4 @@ function maxArea(height) {
 
 }
 
-console.log('Input: ' , 'Expected Output: ', 'Output: ', swapNode());
+console.log('Input: height = [1,8,6,2,5,4,8,3,7]' , 'Expected Output: 49', 'Output: ', swapNode([1,8,6,2,5,4,8,3,7]));
