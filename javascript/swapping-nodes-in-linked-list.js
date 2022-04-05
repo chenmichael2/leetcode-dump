b945cbe8ffb157795c858746d4951b66f53379af
chenmichael2 <chenmichael2012@gmail.com>
2022-04-04 21:15:01 -0700

    when we push a new node, we are making the previous' node's next to be the new Node and redefining the new tail to be the current node

--- javascript/swapping-nodes-in-linked-list.js
+++ javascript/swapping-nodes-in-linked-list.js
@@ -18,7 +18,11 @@ function swapNode(head, k) {
             if (!this.head) {
                 this.head = newNode;
                 this.tail = this.head;
+            } else {
+                this.tail.next = newNode;
+                this.tail = newNode;
             }
+            return this
         }
     }
 }
