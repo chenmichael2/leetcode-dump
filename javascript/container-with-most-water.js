0e15c80efe99f2f2d40d3ec42e72688ab55afa48
chenmichael2 <chenmichael2012@gmail.com>
2022-04-04 21:29:35 -0700

    fixing the function name in the console.log

--- javascript/container-with-most-water.js
+++ javascript/container-with-most-water.js
@@ -4,4 +4,4 @@ function maxArea(height) {
 
 }
 
-console.log('Input: height = [1,8,6,2,5,4,8,3,7]' , 'Expected Output: 49', 'Output: ', swapNode([1,8,6,2,5,4,8,3,7]));
+console.log('Input: height = [1,8,6,2,5,4,8,3,7]' , 'Expected Output: 49', 'Output: ', maxArea([1,8,6,2,5,4,8,3,7]));
