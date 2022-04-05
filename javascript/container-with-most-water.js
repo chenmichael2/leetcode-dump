644862c23e40dbe1405231823941fc779b38de92
chenmichael2 <chenmichael2012@gmail.com>
2022-04-04 21:30:50 -0700

    adding the second consolelog and fixing the input output

--- javascript/container-with-most-water.js
+++ javascript/container-with-most-water.js
@@ -5,3 +5,4 @@ function maxArea(height) {
 }
 
 console.log('Input: height = [1,8,6,2,5,4,8,3,7]' , 'Expected Output: 49', 'Output: ', maxArea([1,8,6,2,5,4,8,3,7]));
+console.log('Input: height = [1,1]' , 'Expected Output: 1', 'Output: ', maxArea([1,1]));
