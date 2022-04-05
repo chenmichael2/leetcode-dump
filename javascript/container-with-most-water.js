63a42838060856e34ccb3e4482e45a5fd67a7eef
chenmichael2 <chenmichael2012@gmail.com>
2022-04-05 16:04:11 -0700

    seeing what x gives

--- javascript/container-with-most-water.js
+++ javascript/container-with-most-water.js
@@ -1,7 +1,11 @@
 // 11 leetcode
 
 function maxArea(height) {
-
+    let x = 0;
+    for (let i = 0; i < height.length; i++) {
+        x += height[i];
+    }
+    return x;
 }
 
 console.log('Input: height = [1,8,6,2,5,4,8,3,7]' , 'Expected Output: 49', 'Output: ', maxArea([1,8,6,2,5,4,8,3,7]));
