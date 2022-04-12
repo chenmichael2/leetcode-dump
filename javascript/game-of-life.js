bb84f9f697a54ddb61427e5d2050db0529dc3ef9
chenmichael2 <chenmichael2012@gmail.com>
2022-04-11 21:39:17 -0700

    adding the function parameter input

--- javascript/game-of-life.js
+++ javascript/game-of-life.js
@@ -4,4 +4,4 @@ function gameofLife() {
 
 }
 
-console.log('Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]' , 'Expected Output: ', 'Output: ', gameofLife());
+console.log('Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]' , 'Expected Output: ', 'Output: ', gameofLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]));
