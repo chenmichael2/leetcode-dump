b0b5997d0a994b749f26a36d2503ebc8cba3a488
chenmichael2 <chenmichael2012@gmail.com>
2022-04-11 21:25:13 -0700

    getting rid of the old info

--- javascript/game-of-life.js
+++ javascript/game-of-life.js
@@ -4,4 +4,4 @@ function gameofLife() {
 
 }
 
-console.log('Input: height = [1,8,6,2,5,4,8,3,7]' , 'Expected Output: 49', 'Output: ', maxArea([1,8,6,2,5,4,8,3,7]));
+console.log('Input: ' , 'Expected Output: ', 'Output: ', maxArea());
