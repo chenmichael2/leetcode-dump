2a371f068eaf84de9684fb068e0b46742408ab4c
chenmichael2 <chenmichael2012@gmail.com>
2022-04-11 22:38:25 -0700

    continuing the problem

--- javascript/game-of-life.js
+++ javascript/game-of-life.js
@@ -8,7 +8,7 @@
 // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 function gameofLife(board) {
     let result = board;
-
+    
 }
 
 console.log('Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]' , 'Expected Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]', 'Output: ', gameofLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]));
