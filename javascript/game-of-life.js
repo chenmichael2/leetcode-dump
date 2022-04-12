50d1821108418e511fbac17e5e423550958f0297
chenmichael2 <chenmichael2012@gmail.com>
2022-04-11 22:40:43 -0700

    adding the explaination

--- javascript/game-of-life.js
+++ javascript/game-of-life.js
@@ -6,9 +6,16 @@
 // Any live cell with two or three live neighbors lives on to the next generation.
 // Any live cell with more than three live neighbors dies, as if by over-population.
 // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+
+// // Translating the upper
+// Any 1 with < 2 neighbors = 0
+// Any 1 with 2 - 3 neighbors = 1
+// Any 1 with > 3 neighbors = 0
+// Any 0 with = 3 neighbors = 1
 function gameofLife(board) {
     let result = board;
-    
+
 }
 
 console.log('Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]' , 'Expected Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]', 'Output: ', gameofLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]));
