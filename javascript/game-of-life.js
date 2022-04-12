ca6ce79c5f7e960b5dc92394b5f0e0bdd9252785
chenmichael2 <chenmichael2012@gmail.com>
2022-04-11 22:14:07 -0700

    making a copy of the board in a new variable

--- javascript/game-of-life.js
+++ javascript/game-of-life.js
@@ -1,7 +1,7 @@
 // 289 leetcode game of life
 
 function gameofLife(board) {
-    
+    let result = board;
 }
 
 console.log('Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]' , 'Expected Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]', 'Output: ', gameofLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]));
