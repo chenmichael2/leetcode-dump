b4b742e811d8c71f49ad31901396abbceab57557
chenmichael2 <chenmichael2012@gmail.com>
2022-04-11 21:48:04 -0700

    adding the specific output

--- javascript/game-of-life.js
+++ javascript/game-of-life.js
@@ -4,4 +4,4 @@ function gameofLife(board) {
 
 }
 
-console.log('Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]' , 'Expected Output: ', 'Output: ', gameofLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]));
+console.log('Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]' , 'Expected Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]', 'Output: ', gameofLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]));
