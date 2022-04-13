4f20889759dc4a6df259f0c9f3337b1c14b4e354
chenmichael2 <chenmichael2012@gmail.com>
2022-04-12 21:37:48 -0700

    adding more space to the parameters

--- javascript/game-of-life.js
+++ javascript/game-of-life.js
@@ -15,7 +15,8 @@
 // Any 0 with = 3 neighbors = 1
 function gameofLife(board) {
     let result = board;
-
+    let neighbors = [];
+    
 }
 
 console.log('Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]' , 'Expected Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]', 'Output: ', gameofLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]));
