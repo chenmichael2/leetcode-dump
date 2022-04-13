8848f5de3e0673da308c17d9f1084b737d1a6279
chenmichael2 <chenmichael2012@gmail.com>
2022-04-12 21:49:18 -0700

    commenting out the second consolelog result

--- javascript/game-of-life.js
+++ javascript/game-of-life.js
@@ -25,4 +25,4 @@ function gameofLife(board) {
 }
 
 console.log('Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]' , 'Expected Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]', 'Output: ', gameofLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]));
-console.log('Input: board = [[1,1],[1,0]]' , 'Expected Output: [[1,1],[1,1]]', 'Output: ', gameofLife([[1,1],[1,0]]));
+// console.log('Input: board = [[1,1],[1,0]]' , 'Expected Output: [[1,1],[1,1]]', 'Output: ', gameofLife([[1,1],[1,0]]));
