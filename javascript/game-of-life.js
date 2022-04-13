d77f7990e88aa97370ca8d4c4146134be2d08a61
chenmichael2 <chenmichael2012@gmail.com>
2022-04-12 21:38:28 -0700

    adding and defining what and how many rows there are

--- javascript/game-of-life.js
+++ javascript/game-of-life.js
@@ -16,7 +16,7 @@
 function gameofLife(board) {
     let result = board;
     let neighbors = [];
-    
+    let rows = board.length;
 }
 
 console.log('Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]' , 'Expected Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]', 'Output: ', gameofLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]));
