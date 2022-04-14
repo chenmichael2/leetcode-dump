6836c1067927070897020f9c36abcb02bcc55906
chenmichael2 <chenmichael2012@gmail.com>
2022-04-13 23:45:39 -0700

    adding a new variable

--- javascript/game-of-life.js
+++ javascript/game-of-life.js
@@ -33,6 +33,7 @@ function gameofLife(board) {
             }
             console.log(target, rowBefore, row, rowAfter);
             // [] grab top, bottom, left, right
+            let 
             // [] grab top right, top left, bottom right, bottom left
             // [] adding the if statements if they don't exist
         }
