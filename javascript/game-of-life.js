b651e886fbfbc88987ac9f71fe4b68806dc21d5c
chenmichael2 <chenmichael2012@gmail.com>
2022-04-15 21:51:14 -0700

    adding the proper comments

--- javascript/game-of-life.js
+++ javascript/game-of-life.js
@@ -98,7 +98,7 @@ function gameofLife(board) {
     return result;
 }
 
-//
+// I need to modify the board not create a new board
 
 console.log('Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]' , 'Expected Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]', 'Output: ', gameofLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]));
 // console.log('Input: board = [[1,1],[1,0]]' , 'Expected Output: [[1,1],[1,1]]', 'Output: ', gameofLife([[1,1],[1,0]]));
