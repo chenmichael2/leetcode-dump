2211a81d86040c65af4b012ccef01d8339908be2
chenmichael2 <chenmichael2012@gmail.com>
2022-04-18 22:41:49 -0700

    adding consolelog content

--- javascript/longest-palindromic-substring.js
+++ javascript/longest-palindromic-substring.js
@@ -5,4 +5,4 @@ function longestPalindrome() {
 
 }
 
-console.log()
+console.log('Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]' , 'Expected Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]', 'Output: ', gameofLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]))
